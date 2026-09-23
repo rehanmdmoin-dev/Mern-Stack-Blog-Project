32211703826bcbe5de231220b249c1d1ff49f51b
Rehan <rehanmdmoin@gmail.com>
2026-09-23 19:43:51 +0530

    Delete rbac-system directory

--- rbac-system/app/utils/logger.js
+++
@@ -1,8 +0,0 @@
-
-const logger = {
-    info: (...args) => console.log('[INFO]', ...args),
-    warn: (...args) => console.warn('[WARN]', ...args),
-    error: (...args) => console.error('[ERROR]', ...args),
-};
-
-module.exports = logger;
